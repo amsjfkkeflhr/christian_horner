df428663229b744e6e187dd7694dc2f3fc1b5e42
amsjfkkeflhr <christian.s.horner@gmail.com>
2020-06-08 16:12:04 -0400

    Update christian_horner.py

--- christian_horner.py
+++ christian_horner.py
@@ -1,5 +1,7 @@
-- company: WWT
-  email: christian.horner@wwt.com
-  first_name: Christian
-  github_username: AMSJFKKEFLHR
-  last_name: Horner
+import yaml
+
+dict_list = [dict (first_name='Christian', last_name='Horner', company='WWT', private_email='christian.s.horner@gmail.com', 
+                   work_email='christian.horner@wwt.com', github_username='AMSJFKKEFLHR']
+
+print (yaml.dump(dict_list))
+                   
