477a48a8fba5156c4ccc7b1c0266768cec56a617
amsjfkkeflhr <christian.s.horner@gmail.com>
2020-06-08 16:20:34 -0400

    Delete christian_horner.py

--- christian_horner.py
+++
@@ -1,7 +0,0 @@
-import yaml
-
-dict_list = [dict (first_name='Christian', last_name='Horner', company='WWT', private_email='christian.s.horner@gmail.com', 
-                   work_email='christian.horner@wwt.com', github_username='AMSJFKKEFLHR']
-
-print (yaml.dump(dict_list))
-                   
